06a5ce4f84727d49638676f642de5613fcd7b9c2
Magicman13000 <dcarlr123@gmail.com>
2023-03-07 22:37:56 -0600

    6 files modified, 4 added, 1 deleted

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var dashboardRouter = require('./routes/dashboard');
+var infoRouter = require('./routes/info');
 
 var app = express();
 
@@ -19,16 +20,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', loginRouter);
+app.use('/dashboard', dashboardRouter);
+app.use('/info', infoRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
